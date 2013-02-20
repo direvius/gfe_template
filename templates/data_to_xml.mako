e83eaf2d9e3e662707b7967b19710145cbe9bce1
Alexey Lavrenuke <direvius@yandex-team.ru>
2013-02-20 16:07:43 +0400

    comparison of charts

--- templates/data_to_xml.mako
+++ templates/data_to_xml.mako
@@ -1,8 +1,11 @@
 % for component in data['data']:
 %  if component['class'] == 'graphite_chart':
-<chart prefix='${component['prefix']}' parameters='${component['parameters']}'>
+<chart prefix='${component['prefix']}'>
 % for target in component['targets']:
-    <target metric='${target['metric']}' function='${target['function']}'/>
+    <target metric='${target['metric']}' />
+% endfor
+% for parameter in component['parameters']:
+    <parameter name='${parameter['name']}' value='${parameter['value']}' />
 % endfor
 </chart>
 %  endif
--- yamltest.py
+++ yamltest.py
@@ -6,5 +6,13 @@ yaml_file = open('yaml/report.yaml', 'r')
 
 data = yaml.safe_load(yaml_file)
 
-mytemplate = Template(filename='templates/data_to_xml.mako', module_directory='mako_modules')
+
+def graphite_chart_url(graphite_chart):
+    metrics = []
+    for target in graphite_chart['targets']:
+        metrics.append("%s.%s" % (graphite_chart['prefix'], target['metric']))
+    targets = '&'.join("target=%s" % metric for metric in metrics)
+    return "http://advmon.yandex.ru/render?%s" % targets
+
+mytemplate = Template(filename='templates/data_to_html.mako', module_directory='mako_modules')
 print mytemplate.render(data=data)
